351ec476bc609522776763a40420aadfbd009d0d
Sittijed Wongtichawat <sittijed.w@arise.tech>
2024-07-21 23:08:41 +0700

    Write test case for Update skill description case success

--- e2e/api/skill.spec.ts
+++ e2e/api/skill.spec.ts
@@ -338,3 +338,71 @@ test.describe('Update skill name', () => {
     await request.delete(apiUrlPrefix + '/skills/python')
   })
 })
+
+test.describe('Update skill description', () => {
+  test('should response status "success" with message "Updating Skill description..." when request PATCH /skills/:key/actions/description', async ({
+    request,
+  }) => {
+    await request.post(apiUrlPrefix + '/skills',
+      {
+        data: {
+          key: 'python',
+          name: 'Python',
+          description: 'Python is an interpreted, high-level, general-purpose programming language.',
+          logo: 'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+          tags: ['programming language', 'scripting']
+        }
+      }
+    )
+    
+    const getResponseBefore = await request.get(apiUrlPrefix + '/skills/python')
+  
+    expect(getResponseBefore.ok()).toBeTruthy()
+    expect(await getResponseBefore.json()).toEqual(
+      expect.objectContaining({
+        status: 'success',
+        data: expect.objectContaining({
+          key: 'python',
+          name: 'Python',
+          description: expect.any(String),
+          logo: expect.any(String),
+          tags: expect.arrayContaining(['programming language', 'scripting']),
+        })
+      })
+    )
+
+    const updateResponse = await request.patch(apiUrlPrefix + '/skills/python/actions/description',
+      {
+        data: {
+          description: 'Python 3 is the latest version of Python programming language.',
+        }
+      }
+    )
+
+    expect(updateResponse.ok()).toBeTruthy()
+    expect(await updateResponse.json()).toEqual(
+      expect.objectContaining({
+        status: 'success',
+        message: 'Updating Skill description...'
+      })
+    )
+
+    const getResponseAfter = await request.get(apiUrlPrefix + '/skills/python')
+  
+    expect(getResponseAfter.ok()).toBeTruthy()
+    expect(await getResponseAfter.json()).toEqual(
+      expect.objectContaining({
+        status: 'success',
+        data: expect.objectContaining({
+          key: 'python',
+          name: expect.any(String),
+          description: 'Python 3 is the latest version of Python programming language.',
+          logo: expect.any(String),
+          tags: expect.any(Array),
+        })
+      })
+    )
+
+    await request.delete(apiUrlPrefix + '/skills/python')
+  })
+})
